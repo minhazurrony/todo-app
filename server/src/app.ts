948def4af7756eb182382ed46b967fa0156ef3c3
Minhazur Rahman <minhazur.rony@gmail.com>
2024-10-20 23:20:27 +0600

    feat(server): add register api

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import { registerController } from "./controller";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Middleware for parsing JSON bodies
+app.use(express.json());
+
+app.post("/api/register", registerController);
 
 app.listen(port, () => {
   console.log(`todo-app listening on port ${port}`);
