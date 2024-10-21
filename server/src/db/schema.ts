077064c2b48fda90829d825d2bed14a8d8a61dc3
Minhazur Rahman <minhazur.rony@gmail.com>
2024-10-22 00:08:38 +0600

    chore(server): update db schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,11 +1,11 @@
-import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, varchar, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().default("gen_random_uuid()"),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar().notNull(),
-  username: varchar({ length: 255 }).notNull(),
+  username: varchar({ length: 255 }).notNull().unique(),
   created_at: timestamp().notNull().defaultNow(),
   updated_at: timestamp().notNull().defaultNow(),
 });
