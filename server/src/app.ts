e792a45823dc086220c7750ec398de864492c54d
Minhazur Rahman <minhazur.rony@gmail.com>
2024-11-12 15:21:54 +0600

    feat(server): add users api route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv";
 import { loginController, registerController } from "./controller";
+import { verifyToken } from "./middleware";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -8,8 +10,15 @@ const port = process.env.PORT || 3001;
 // Middleware for parsing JSON bodies
 app.use(express.json());
 
+// Middleware for parsing cookie
+app.use(cookieParser());
+
 app.post("/api/auth/register", registerController);
 app.post("/api/auth/login", loginController);
+
+app.get("/api/users", verifyToken, (req, res) => {
+  res.send(200);
+});
 
 app.listen(port, () => {
   console.log(`todo-app listening on port ${port}`);
