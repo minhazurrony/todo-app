fd94e4ca23067f7e415659aa785af0b0b506feed
Minhazur Rahman <minhazur.rony@gmail.com>
2024-11-12 18:09:37 +0600

    chore(server): add todos table schema

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,4 +1,10 @@
-import { pgTable, varchar, timestamp, integer } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  varchar,
+  timestamp,
+  integer,
+  boolean,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,6 +12,15 @@ export const usersTable = pgTable("users", {
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar().notNull(),
   username: varchar({ length: 255 }).notNull(),
+  created_at: timestamp().notNull().defaultNow(),
+  updated_at: timestamp().notNull().defaultNow(),
+});
+
+export const todosTable = pgTable("todos", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  todo: varchar({ length: 255 }).notNull(),
+  completed: boolean().default(false),
+  user_id: integer().notNull(),
   created_at: timestamp().notNull().defaultNow(),
   updated_at: timestamp().notNull().defaultNow(),
 });
